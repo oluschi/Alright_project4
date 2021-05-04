71ae0cfdfcf86eb6a13cc4485ab59d11a5663ca1
oluschi <72060680+oluschi@users.noreply.github.com>
2021-05-04 04:44:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+// About Page PopUp //
+function aboutPopUp() {
+  var about = document.getElementById("aboutPop");
+  about.classList.toggle("show");
+  //popupAbout.classList.toggle("show");//
+
+// About Draggle function//
+//  var aboutdrag = document.addEventListener("click",function(){ //
+
+}
+
+
+$( function () {
+    $( ".draggable" ).draggable();
+  } );
+  
+function gifPopUp(){
+	var gif =document.getElementById("gifPop");
+	gif.classList.toggle("show")
+}
+// Share Form PopUp //
+/*function sharePopUp() {
+  console.log("you clicked!");
+  var popupShare = document.getElementById("share");
+  popupShare.classList.toggle("show");
+  console.log("you got to the end of the function!");
+};*/
+
+
